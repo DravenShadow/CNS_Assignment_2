3c0d34bc14103115b0c3574829da77d29ff5e5c5
DravenPredator <depree.rowland@gmail.com>
2016-02-27 21:53:30 -0500

    Modify to use threading to send udp packets

--- sniffer.py
+++ sniffer.py
@@ -1,40 +1,72 @@
 import os
 import socket
+import threading
+import time
 from _ctypes import *
+
+from netaddr import IPNetwork, IPAddress
 
 import ICMP
 from IP import IP
 
 host = "192.168.1.7"
 
-if os.name == "nt":
-    socket_protocol = socket.IPPROTO_IP
-else:
-    socket_protocol = socket.IPPROTO_ICMP
+subnet = "192.168.0.0/24"
 
-sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+magic_message = "PYTHONRULES!"
 
-sniffer.bind((host, 0))
 
-sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+def udp_sender(subnet, magic_message):
+    time.sleep(5)
+    sender = socket.socket(socket.AF_INENT, socket.SOCK_DGRAM)
 
-if os.name == "nt":
-    sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
-try:
-    while True:
-        raw_buffer = sniffer.recvfrom(65565)[0]
+    for ip in IPNetwork(subnet):
+        try:
+            sender.sendto(magic_message, ("%s" % ip, 65212))
+        except:
+            pass
 
-        ip_header = IP(raw_buffer[0:20])
 
-        print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
-
-        if ip_header.protocol == "ICMP":
-            offset = ip_header.ihl * 4
-            buf = raw_buffer[offset:offset + sizeof(ICMP)]
-
-            icmp_header = ICMP(buf)
-
-            print("ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code))
-except KeyboardInterrupt:
+def main():
     if os.name == "nt":
-        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+        socket_protocol = socket.IPPROTO_IP
+    else:
+        socket_protocol = socket.IPPROTO_ICMP
+
+    sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+
+    sniffer.bind((host, 0))
+
+    sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+    if os.name == "nt":
+        sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+    try:
+        while True:
+            raw_buffer = sniffer.recvfrom(65565)[0]
+
+            ip_header = IP(raw_buffer[0:20])
+
+            print("Protocol: %s %s -> %s" % (ip_header.protocol, ip_header.src_address, ip_header.dst_address))
+
+            if ip_header.protocol == "ICMP":
+                offset = ip_header.ihl * 4
+                buf = raw_buffer[offset:offset + sizeof(ICMP)]
+
+                icmp_header = ICMP(buf)
+
+                print("ICMP -> Type: %d Code: %d" % (icmp_header.type, icmp_header.code))
+
+                if icmp_header.code == 3 and icmp_header.type == 3:
+                    if IPAddress(ip_header.src_address) in IPNetwork(subnet):
+                        if raw_buffer[len(raw_buffer) - len(magic_message):] == magic_message:
+                            print("Host Up: %s" % ip_header.src_address)
+    except KeyboardInterrupt:
+        if os.name == "nt":
+            sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+
+if __name__ == "__main__":
+    t = threading.Thread(target=udp_sender, args=(subnet, magic_message))
+    t.start()
+    main()
