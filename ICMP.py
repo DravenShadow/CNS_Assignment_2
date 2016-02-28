dfffdbcd50543657047f01f5af656ed6a9df7a72
DravenPredator <depree.rowland@gmail.com>
2016-02-28 12:32:18 -0500

    Added comments

--- ICMP.py
+++ ICMP.py
@@ -1,7 +1,19 @@
+"""
+        Author : Rowland DePree             ICMP.py
+
+        A program designed to form the structure of an ICMP packet
+"""
+
 from ctypes import *
 
 
 class ICMP(Structure):
+    """
+    A class to structure an ICMP packet
+
+    :param Structure:
+    :return:
+    """
     _fields_ = [
         ("type", c_ubyte),
         ("code", c_ubyte),
@@ -11,7 +23,17 @@ class ICMP(Structure):
     ]
 
     def __init__(self, socket_buffer):
+        """
+        Constructor
+        :param socket_buffer:
+        :return:
+        """
         pass
 
     def __new__(self, socket_buffer):
+        """
+        Forms the structure of the packet from the parameter
+        :param socket_buffer:
+        :return:
+        """
         return self.from_buffer_copy(socket_buffer)
